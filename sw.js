70522597635144ad417b938ae895bff45c26a7fe
Smalluniverseheng <1829487897@qq.com>
2026-07-30 13:50:54 +0800

    v7.0: create service worker

---
+++ sw.js
@@ -0,0 +1,30 @@
+const VERSION = 'v7.0';
+const CACHE_NAME = 'omnihub-' + VERSION;
+
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/store.js',
+  '/js/app.js',
+  '/js/modules/nav.js',
+  '/js/modules/profile.js',
+  '/js/modules/read.js',
+  '/js/modules/reader.js',
+  '/js/venera-engine.js',
+  '/js/providers.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
